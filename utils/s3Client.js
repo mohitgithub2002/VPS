836fc6e995335ab6fa9def75bc3064fe650dc87d
Mohit Goyal <mohit.work2002@gmail.com>
2025-08-13 12:31:45 +0530

    Feat: Implement daily and exam schedule in pdf or image format

---
+++ utils/s3Client.js
@@ -0,0 +1,14 @@
+import { S3Client } from '@aws-sdk/client-s3';
+
+const region = process.env.AWS_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+export const s3 = new S3Client({
+  region,
+  credentials: accessKeyId && secretAccessKey ? { accessKeyId, secretAccessKey } : undefined,
+});
+
+console.log('s3 is connected');
+
+
